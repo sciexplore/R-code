2a88da746ec1348aac75cc8832f0279937e083f2
sciexplore <grinetti@hotmail.com>
2021-06-14 17:13:54 -0700

    Add files via upload
    generalized command lines and added fread

--- R_open_ datasets.R
+++ R_open_ datasets.R
@@ -1,24 +1,29 @@
 #install.packages('package_name', dependencies=TRUE, repos='http://cran.rstudio.com/') 
 
 
-# open sas file
+# sas -- open file
 library(haven)
-setwd("/bd-fs-mnt/sasgprod_home/o993927/nas/care_plus/data/Datasets_Bruce/CP_Bruce_dataset_2020_1102_MATCHED_CONTROLS_&_OTX")  
-cc = as.data.frame(read_sas("cp_stable_otx_cases_comps.sas7bdat")) # if I do not open SAS file as.data.frame I will import empty rows/cells as well  
+setwd("home/dir/proj/data/subdir")  
+cc = as.data.frame(read_sas("dataset.csv.sas7bdat")) # if I do not open SAS file as.data.frame I will import empty rows/cells as well  
 
 
-# save and open csv file
+# csv -- save and open files
 library(readr)
 #or library(tidyverse)
-
-setwd("~/sasgprod_home/nas/care_plus/data/cases_controls_cp") 
+setwd("~/home/dir/proj/data/subdir") 
 #save
-write_csv(cc_mrn, "CASES_CONTROLS_MRNs.csv") 
+write_csv(dataset, "dataset.csv") 
 # open 
-cc_mrn = read_csv("CASES_CONTROLS_MRNs.csv") 
+ds = read_csv("dataset.csv") 
 
 
-# save and open rds file
-saveRDS(cc_mrn, "CASES_CONTROLS_MRNs.rds") 
-cc_mrn = readRDS("CASES_CONTROLS_MRNs.rds") 
+# rds -- save and open files
+saveRDS(dataset, "dataset.rds") 
+ds = readRDS("dataset.rds") 
+
+
+
+# fread-- open files
+library(data.table) # open fread files
+ds = fread("dataset.csv")
 
